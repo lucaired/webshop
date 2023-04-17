dc22422da8a5bf4779c64fff375b8c1abc2e5028
lucaired <70109317+lucaired@users.noreply.github.com>
2023-04-17 12:05:56 +0200

    Add firebase authentication

--- src/routes/SignIn/SignIn.tsx
+++ src/routes/SignIn/SignIn.tsx
@@ -1,7 +1,16 @@
+import { UserCredential } from "firebase/auth";
+import { createUserDocFromAuth, signInWithGooglePopup } from "../../utils/firebase";
+
 const SignIn = () => {
+    const logGoogleIn = async () => {
+        const userCredential: UserCredential = await signInWithGooglePopup();
+        const userDoc = await createUserDocFromAuth(userCredential.user, {});
+        console.log(userDoc);
+    };
     return (
         <div>
             <h1>Sign In</h1>
+            <button onClick={logGoogleIn}>Sign in with Google</button>
         </div>
     );
 }
---
+++ src/utils/firebase.tsx
@@ -0,0 +1,66 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
+
+/** Authentication */
+
+// TODO: Replace the following with your app's Firebase project configuration
+// How to add react environment variables to vs code ?
+// https://stackoverflow.com/questions/56238356/using-environment-variables-in-react
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+}
+  
+const firebase = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+// Force account selection even when one account
+// is available.
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const auth = getAuth(firebase);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+interface UserAuth {
+    uid?: any;
+    displayName?: any;
+    email?: any;
+}
+
+
+/** Firestore */
+
+export const db = getFirestore(firebase);
+
+/** Storage */
+
+export const createUserDocFromAuth = async (userAuth: UserAuth, additionalData: any) => {
+    if (!userAuth) return;
+
+    const userRef = doc(db, 'users', userAuth.uid);
+    const snapShot = await getDoc(userRef);
+
+    if (!snapShot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error);
+        }
+    }
+    return userRef;
+}
