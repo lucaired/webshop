37bbb56031b9cf48e207a7cda3d80b484e1a0fac
lucaired <70109317+lucaired@users.noreply.github.com>
2023-04-17 10:48:22 +0200

    Add user context

---
+++ src/Contexts/UserContext.tsx
@@ -0,0 +1,31 @@
+import {createContext, useState} from 'react';
+
+interface User {
+    name: string;
+    email: string;
+    isLoggedIn: boolean;
+}
+
+export const UserContext = createContext<{
+    user: User | null,
+    setUser: (user: User) => void
+}
+>({
+    user: null,
+    setUser: (user: User) => {}
+});
+
+interface UserContextProviderProps {
+    children: React.ReactNode;
+}
+
+export const UserContextProvider = (props: UserContextProviderProps) => {
+    const [user, setUser] = useState<User | null>(null);
+    const {children} = props;
+
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    );
+}
