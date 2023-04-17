33331e15f263a3b3cc6717452410f1cc1addb4f7
lucaired <70109317+lucaired@users.noreply.github.com>
2023-04-17 09:21:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="categories-container">
+      <div className="category-body-container">
+        <h1>Shoes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
