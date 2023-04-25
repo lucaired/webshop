bce4c43cdfa5458cb6ee7ff076e145b16055a13a
lucaired <70109317+lucaired@users.noreply.github.com>
2023-04-25 15:31:42 +0200

    Block routes when certain conditions dont meet

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home/Home';
@@ -5,19 +7,25 @@ import NavBar from './routes/NavBar/NavBar';
 import SignIn from './routes/SignIn/SignIn';
 import Shop from './components/Shop/Shop';
 import Checkout from './components/Checkout/Checkout';
+import { LocalUserContext } from './contexts/UserContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
-  // TODO: make some routes unaccessible
+  const { localUser } = useContext(LocalUserContext);
+  const { cartItemsCount} = useContext(CartContext);
+
   return (
     <Routes>
       <Route path='/' element={<NavBar />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
-        <Route path='checkout' element={<Checkout />} />
+        <Route path='sign-in' element={!localUser ? <SignIn /> : <Home />} />
+        <Route path='checkout' element={cartItemsCount > 0 ? <Checkout /> : <Home />} />
       </Route>
     </Routes>
   );
 }
 
 export default App;
+
+
