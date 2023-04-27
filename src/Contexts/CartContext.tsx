42b7ebfcd686510a66e7c67abf092982b84bc166
lucaired <70109317+lucaired@users.noreply.github.com>
2023-04-27 17:38:38 +0200

    Restructure CartContext

--- src/Contexts/CartContext.tsx
+++ src/Contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import React, { Reducer, createContext, useEffect, useReducer } from "react";
+import React, { Reducer, createContext, useReducer } from "react";
 import { Product } from "./CategoryContext";
 
 export class CartItem {
@@ -11,57 +11,121 @@ export class CartItem {
     }
 }
 
-function increaseItemQuanitity(items: CartItem[], index: number): CartItem[] {
+function changeItemQuanitity(items: CartItem[], index: number, delta: number): CartItem[] {
+    /**
+     * If new quantity is negative, remove the item if the quantity is 1 or more than apply the delta
+     */
+    
+    const currentItem = items[index];
+    if (currentItem.quantity + delta <= 0) return items.filter((currentItem, currentIndex) => currentIndex !== index);
+
     return items.map((currentItem, currentIndex) => {
         return (currentIndex === index)
-            ? new CartItem(currentItem.product, currentItem.quantity + 1)
+            ? new CartItem(currentItem.product, currentItem.quantity + delta)
             : currentItem;
     });
 }
 
-type updateCartItemsAction = 'ADD_CART_ITEM' | 'REMOVE_CART_ITEM' | 'SET_CART_ITEM_QUANTITY' | 'INCREMENT_CART_ITEM_QUANTITY' | 'CLEAR_CART';
+function setItemQuanitity(items: CartItem[], index: number, quantity: number): CartItem[] {
+    /**
+     * If new quantity is negative, remove the item if the quantity is 1 or more than set the quantity
+     */
 
-const updateCartItems = (target: Product, items: CartItem[], action: updateCartItemsAction, payload?: number): CartItem[] => {
+    if (quantity <= 0) return items.filter((currentItem, currentIndex) => currentIndex !== index);
+    return items.map((currentItem, currentIndex) => {
+        return (currentIndex === index)
+            ? new CartItem(currentItem.product, quantity)
+            : currentItem;
+    });
+}
+
+type updateCartItemsAction = 'ADD_CART_ITEM' | 'REMOVE_CART_ITEM' | 'SET_CART_ITEM_QUANTITY' | 'CHANGE_CART_ITEM_QUANTITY' | 'CLEAR_CART';
+
+const updateCartItems = (target: Product | null, state: CartState, action: updateCartItemsAction, payload?: number): {
+    cartItems: CartItem[],
+    isCartHidden: boolean,
+    cartItemsCount: number,
+    cartTotal: number
+} => {
+    
     if (action === 'CLEAR_CART') {
-        return [];
-    } else {
-        const index = items.findIndex((currentItem) => currentItem.product.id === target.id);
-        let newItems: CartItem[] = items;
 
-        if (index !== -1) {
-            if (action === 'ADD_CART_ITEM') {
-                newItems = increaseItemQuanitity(items, index)
-            } else if (action === 'REMOVE_CART_ITEM') {
-                newItems = [...items.filter((currentItem) => currentItem.product.id !== target.id)];
+        return {
+            cartItems: [],
+            isCartHidden: true,
+            cartItemsCount: 0,
+            cartTotal: 0
+        };
 
-            } else if (action === 'SET_CART_ITEM_QUANTITY') {
-                if (payload !== undefined && payload > 0) {
-                    newItems = items.map((currentItem, currentIndex) => {
-                            return (currentIndex === index) 
-                            ? new CartItem(currentItem.product, payload)
-                            : currentItem;
-                        });
-                }
-            } else if (action === 'INCREMENT_CART_ITEM_QUANTITY') {
-                if (payload !== undefined && payload > 0) {
-                    newItems = items.map((currentItem, currentIndex) => {
-                            return (currentIndex === index) 
-                            ? new CartItem(currentItem.product, currentItem.quantity + payload)
-                            : currentItem;
-                    });
-                } 
-            }
-        } else {
-            if (action === 'ADD_CART_ITEM') {
-                newItems = [...items, new CartItem(target, 1)];
-            }
-        }
-        return newItems;
     }
+
+    if (target === null || target === undefined) return state;
+
+    const index = state.cartItems.findIndex((currentItem) => currentItem.product.id === target.id);
+    let newItems: CartItem[] = state.cartItems;
+    let cartItemsCount = 0;
+    let cartTotal = 0;
+
+    if (index !== -1) {
+        
+        if (action === 'ADD_CART_ITEM') {
+            newItems = changeItemQuanitity(state.cartItems, index, 1);
+            cartItemsCount = state.cartItemsCount + 1;
+            cartTotal = state.cartTotal + target.price;
+
+        } else if (action === 'REMOVE_CART_ITEM') {
+            newItems = [...state.cartItems.filter((currentItem) => currentItem.product.id !== target.id)];
+            cartItemsCount = state.cartItemsCount -1 * state.cartItems[index].quantity;
+            cartTotal = state.cartTotal -1 * state.cartItems[index].quantity * target.price;
+
+        } else if (action === 'SET_CART_ITEM_QUANTITY') {
+            if (payload !== undefined) {
+                newItems = setItemQuanitity(state.cartItems, index, payload);
+
+                if (payload <= 0) {
+                    // remove the old item information
+                    cartItemsCount = state.cartItemsCount -1 * state.cartItems[index].quantity;
+                    cartTotal = state.cartTotal -1 * state.cartItems[index].quantity * target.price;
+                } else {
+                    // add the new information and remove the old information
+                    cartItemsCount = state.cartItemsCount + payload - state.cartItems[index].quantity;
+                    cartTotal = (payload - state.cartItems[index].quantity) * target.price;
+                }
+            }
+        } else if (action === 'CHANGE_CART_ITEM_QUANTITY') {
+            if (payload !== undefined) {
+
+                newItems = changeItemQuanitity(state.cartItems, index, payload);
+                
+                if (payload + state.cartItems[index].quantity <= 0) {
+                    // remove the old item information
+                    cartItemsCount = state.cartItemsCount -1 * state.cartItems[index].quantity;
+                    cartTotal = -1 * state.cartItems[index].quantity * target.price;
+                } else {
+                    // just apply the delta
+                    cartItemsCount = state.cartItemsCount + payload;
+                    cartTotal = state.cartItemsCount + payload * target.price;
+                }
+            } 
+        }
+    } else {
+        if (action === 'ADD_CART_ITEM') {
+            newItems = [...state.cartItems, new CartItem(target, 1)];
+            cartItemsCount = state.cartItemsCount + 1;
+            cartTotal = state.cartItemsCount + target.price;
+        }
+    }
+    
+    return {
+        cartItems: newItems,
+        isCartHidden: state.isCartHidden,
+        cartItemsCount: cartItemsCount,
+        cartTotal: cartTotal
+    };
 }
 
 export type CART_ACTION_TYPES = {
-    type: 'ADD_CART_ITEM' | 'REMOVE_CART_ITEM' | 'SET_CART_ITEM_QUANTITY' | 'INCREMENT_CART_ITEM_QUANTITY' | 'SET_IS_CART_HIDDEN' | 'SET_CART_TOTAL' | 'SET_CART_ITEMS_COUNT',
+    type: 'ADD_CART_ITEM' | 'REMOVE_CART_ITEM' | 'SET_CART_ITEM_QUANTITY' | 'CHANGE_CART_ITEM_QUANTITY' | 'SET_IS_CART_HIDDEN' | 'CLEAR_CART',
     payload: number | Product | CartItem | boolean | { product: Product, quantity: number } | { product: Product, delta: number }
 }
 
@@ -82,40 +146,20 @@ const initialState: CartState = {
 export const cartReducer: Reducer<CartState, CART_ACTION_TYPES> = (state: CartState, action: CART_ACTION_TYPES) => {
     switch (action.type) {
         case 'ADD_CART_ITEM':
-            return {
-                ...state,
-                cartItems: updateCartItems(action.payload as Product, state.cartItems, 'ADD_CART_ITEM')
-            };
+            return updateCartItems(action.payload as Product, state, 'ADD_CART_ITEM');
         case 'REMOVE_CART_ITEM':
-            return {
-                ...state,
-                cartItems: updateCartItems(action.payload as Product, state.cartItems, 'REMOVE_CART_ITEM')
-            };
+            return updateCartItems(action.payload as Product, state, 'REMOVE_CART_ITEM');
         case 'SET_CART_ITEM_QUANTITY':
-            return {
-                ...state,
-                cartItems: updateCartItems((action.payload as CartItem).product, state.cartItems, 'SET_CART_ITEM_QUANTITY', (action.payload as CartItem).quantity)
-            };
-        case 'INCREMENT_CART_ITEM_QUANTITY':
-            return {
-                ...state,
-                cartItems: updateCartItems((action.payload as CartItem).product, state.cartItems, 'INCREMENT_CART_ITEM_QUANTITY', (action.payload as CartItem).quantity)
-            };
+            return updateCartItems((action.payload as CartItem).product, state, 'SET_CART_ITEM_QUANTITY', (action.payload as CartItem).quantity);
+        case 'CHANGE_CART_ITEM_QUANTITY':
+            return updateCartItems((action.payload as CartItem).product, state, 'CHANGE_CART_ITEM_QUANTITY', (action.payload as CartItem).quantity);
         case 'SET_IS_CART_HIDDEN':
             return {
                 ...state,
                 isCartHidden: action.payload as boolean
             };
-        case 'SET_CART_ITEMS_COUNT':
-            return {
-                ...state,
-                cartItemsCount: action.payload as number
-            };
-        case 'SET_CART_TOTAL':
-            return {
-                ...state,
-                cartTotal: action.payload as number
-            };
+        case 'CLEAR_CART':
+            return updateCartItems(null, state, 'CLEAR_CART');
         default:
             console.error(`Unhandled type ${action.type} in userReducer`);
             return state;
@@ -154,21 +198,6 @@ export const CartContextProvider = (props: CartContextProviderProps) => {
 
     const [ {cartItems, isCartHidden, cartItemsCount, cartTotal }, dispatch ] = useReducer(cartReducer, initialState);
 
-    /*
-    useEffect(() => {
-        // TODO: bring this functionality into the reducer
-        const count = cartItems.reduce((total: number, cartItem: CartItem) => total += cartItem.quantity, 0);
-        dispatch({ type: 'SET_CART_ITEMS_COUNT', payload: count });
-        
-        if (count === 0) {
-            dispatch({ type: 'SET_IS_CART_HIDDEN', payload: true });
-        }
-
-        dispatch({ type: 'SET_CART_TOTAL', payload: cartItems.reduce((total: number, cartItem: CartItem) => total += cartItem.quantity * cartItem.product.price, 0) });
-
-    }, [cartItems])
-    */
-
     const addCartItem = (cartItem: Product) => {
         dispatch({ type: 'ADD_CART_ITEM', payload: cartItem });
     }
@@ -182,7 +211,7 @@ export const CartContextProvider = (props: CartContextProviderProps) => {
     }
 
     const incrementCartItemQuantity = (cartItem: Product, delta: number) => {
-        dispatch({ type: 'INCREMENT_CART_ITEM_QUANTITY', payload: { product: cartItem, delta } });
+        dispatch({ type: 'CHANGE_CART_ITEM_QUANTITY', payload: { product: cartItem, delta } });
     }
 
     const setIsCartHidden = (isCartHidden: boolean) => {
